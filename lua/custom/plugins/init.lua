f5eeb26e7934b0abd7902944211756035a953ac6
teodord25 <djuric.teodor25@gmail.com>
2024-04-06 13:21:26 +0200

    add plugins file; add disable netrw at top of init for nvimtree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,43 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
+-- TODO: add lazy event trigger stuff
 --
--- See the kickstart.nvim README for more information
-return {}
+
+return {
+
+  { 'mbbill/undotree' },
+
+  { 'airblade/vim-gitgutter' },
+
+  { 'github/copilot.vim' },
+
+  { 'christoomey/vim-tmux-navigator' },
+
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+  },
+
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^4', -- Recommended
+    ft = { 'rust' },
+  },
+
+  {
+    'nvim-tree/nvim-tree.lua',
+    config = true,
+    opts = {},
+  },
+
+  {
+    'lervag/vimtex',
+    -- loaded when a tex file is opened
+    ft = { 'tex' },
+  },
+}
